d0d3c8bee88a37c49c84cae2659849beb633352c
tspanelpro <63020638+tspanelpro@users.noreply.github.com>
2020-04-02 00:47:26 +0300

    Örnek Api işlemleri

---
+++ examples/createAccountLogin.php
@@ -0,0 +1,33 @@
+<?php 
+include '../lib/tspanel.class.php';
+$apikey="YourApiKey";
+$secret="YourApiSecretKey";
+
+$api = new tspanel_api("https://www.tspanel.pro/api/"); // Bağlantı Sağlıyoruz
+$api->SetApi($apikey,$secret); // Api kimliğimizi tanımlıyoruz
+$api->SetAuth(1,"bayi@tspanel.pro"); // Bayi Hesabımızı Doğruluyoruz
+$api->SetHeader(); // Gerekli Bilgileri Header ile Bağlantı Adresine Gönderiyoruz
+
+// Tek Seferlik Giriş Kodu Oluşturuyoruz
+$api->SetAction('account_login'); // Session Oluşturacağımızı Belirttik
+$api->SetParams(array("api_hash" =>"apihashtest123412341234")); // Gerekli bilgileri güncelliyoruz
+$response = $api->send(); // Bağlantıyı sağlıyoruz ve post ediyoruz
+$code=$response['data']; // Session Kodumuz
+?>
+
+<?php 
+if ($response['success'] == 1) {
+?>
+	<p>Oturum Kodunuz: <?php echo $code; ?></p>
+	<a href="https://www.tspanel.pro/api/session/<?php echo $code; ?>">Giriş yapmak için tıklayınız</a>
+<?php 
+}else if($response['api_error'] == 1){
+?>
+    <div class="alert alert-danger"><?php echo $response['message']['api_error']; ?> | Hata Kodu:<?php echo $response['code']; ?></div>
+<?php 
+}else{
+?>
+    <div class="alert alert-danger">cURL Error: <?php echo $response['message']['curl_error']; ?></div>
+<?php 
+}
+?>
---
+++ examples/createUser.php
@@ -0,0 +1,32 @@
+<?php 
+include '../lib/tspanel.class.php';
+$apikey="YourApiKey";
+$secret="YourApiSecretKey";
+
+$api = new tspanel_api("https://www.tspanel.pro/api/"); // Bağlantı Sağlıyoruz
+$api->SetApi($apikey,$secret); // Api kimliğimizi tanımlıyoruz
+$api->SetAuth(1,"bayi@tspanel.pro"); // Bayi Hesabımızı Doğruluyoruz
+$api->SetHeader(); // Gerekli Bilgileri Header ile Bağlantı Adresine Gönderiyoruz
+
+// Müşteri Hesabı Oluşturuyoruz
+$api->SetAction('user_create'); // Müşteri Hesabı Oluşturacağımızı Belirttik
+$api->SetParams(array("email" => "test@test.com", "name"=>"Test", "surname"=>"Test", "password"=>"test", "status" => 0)); // Gerekli bilgileri güncelliyoruz
+$response = $api->send(); // Bağlantıyı sağlıyoruz ve post ediyoruz
+$data=$response['data']; // Geri dönen verileri alıyoruz
+?>
+
+<?php 
+if ($response['success'] == 1) {
+?>
+	<p>Oluşturulan Hesap: <?php echo $data['email']; ?></p>
+<?php 
+}else if($response['api_error'] == 1){
+?>
+    <div class="alert alert-danger"><?php echo $response['message']['api_error']; ?> | Hata Kodu:<?php echo $response['code']; ?></div>
+<?php 
+}else{
+?>
+    <div class="alert alert-danger">cURL Error: <?php echo $response['message']['curl_error']; ?></div>
+<?php 
+}
+?>
